c4036b0f1cbafadbd629479b19bb5a40333d83c5
atef <atefbuabid664@gmail.com>
2021-03-07 18:12:03 +0100

    add to github

---
+++ src/main/java/com/atef/spring/springJdbc/App.java
@@ -0,0 +1,13 @@
+package com.atef.spring.springJdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
